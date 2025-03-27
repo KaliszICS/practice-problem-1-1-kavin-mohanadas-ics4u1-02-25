fd4a06fbb34670a3bf913449f033e103118abb23
kavin-mohanadas-ics4u1-02-25 <kavin.mohanadas@student.tdsb.on.ca>
2025-03-27 20:08:22 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,28 +1,37 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static boolean validIndex(int[] intArray, int index) {
+		try {
+			int temp = intArray[index];
+			return true;
+
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static int divide(int one, int two) {
+		try {
+			return one/two;
+		} catch(ArithmeticException e) {
+			return 0;
+		}
+		
 	}
 
-	public static void q3() {
+	public static int safeConvertStringtoInt(String string) {
+		try {
+			return Integer.parseInt(string);
+		} catch (NumberFormatException e) {
+			return 0;
+		}
 		//Write question 3 code here
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
 
 	public static int sum2D(int[][] arr) {
 		int sum = 0;
